46bde284c464cb618e3ae19545436962086ae27b
Tousif-hafiz <tousifullah58802@gmail.com>
2023-09-18 10:59:17 +0530

    this commit hava done for second section which is about me

--- functionality.js
+++ functionality.js
@@ -8,4 +8,34 @@ can.addEventListener("click",()=>{
     menu.style.display="none"
     
 })
+let skill1=document.getElementById("skills")
+let education1=document.getElementById("education")
+let experience1=document.getElementById("experience")
+let s_active=document.getElementById("s-active")
+let ed_active=document.getElementById("ed-active")
+let e_active=document.getElementById("ex-active")
+function skills(){
+    skill1.style.display="block"
+    education1.style.display="none"
+    experience1.style.display="none"
+    ed_active.style.color="black"
+    s_active.style.color="purple"
+    e_active.style.color="black"
+}
+function education(){
+    education1.style.display="block"
+    experience1.style.display="none"
+    skill1.style.display="none"
+    ed_active.style.color="purple"
+    s_active.style.color="black"
+    e_active.style.color="black"
 
+}
+function experience(){
+    experience1.style.display="block"
+    skill1.style.display="none"
+    education1.style.display="none"
+    s_active.style.color="black"
+    ed_active.style.color="black"
+    e_active.style.color="purple"
+}
